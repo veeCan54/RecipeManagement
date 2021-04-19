ed110f42c1578092b9ee5315c4e38a5c56dfb841
vidya_z <vidya_z@vidya.com>
2021-04-19 15:41:03 -0400

    Adding lombak dependency and code changes

--- recipe-management/src/main/java/com/practise/recipemanagement/recipe/Recipe.java
+++ recipe-management/src/main/java/com/practise/recipemanagement/recipe/Recipe.java
@@ -46,45 +46,6 @@ public class Recipe {
 		this.title = title;
 		this.instructions = instructions;
 		this.user = user;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	/*public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public String getInstructions() {
-		return instructions;
-	}
-
-	public void setInstructions(String instructions) {
-		this.instructions = instructions;
-	}
-
-	public List<RecipeIngredient> getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(List<RecipeIngredient> ingredients) {
-		this.ingredients = ingredients;
-	}*/
+	}	
+	
 }
